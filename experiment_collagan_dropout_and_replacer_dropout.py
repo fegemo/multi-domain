3e402c5f582c815ae9e2246862d5fd71bd81469f
Flávio Coutinho <fegemo@gmail.com>
2024-05-27 18:34:29 -0300

    Changes experiment to what remains to be calculated for SBGames24

--- experiment_collagan_dropout_and_replacer_dropout.py
+++ experiment_collagan_dropout_and_replacer_dropout.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
                 "all", "no-tran"
             ],
             "log-folder": config.output if config.output is not None else "output",
-            "steps": 80000,
+            "steps": 240000,
             "evaluate-steps": 1000,
-            "capacity": 4,
+            # "capacity": 4,
             "lr": 0.0001,
             "batch": 4,
             "lambda-l1": 100,
@@ -28,41 +28,43 @@ if __name__ == "__main__":
             "lambda-domain": 10,
             "lr-decay": "constant-than-linear",
             "model-name": "@model",
-            "experiment": "all,&input-dropout,&cycled-source-replacer"
+            "experiment": "all,&input-dropout,&cycled-source-replacer&capacity"
         }, {
-            "input-dropout": ["none", "original", "aggressive", "balanced", "conservative", "curriculum"],
-            "cycled-source-replacer": ["dropout"]
+            # "input-dropout": ["none", "original", "aggressive", "balanced", "conservative", "curriculum"],
+            "input-dropout": ["original"],
+            "cycled-source-replacer": ["dropout"],
+            "capacity": [1, 4]
         })
 
     logging.info("Starting execution of the dropout and replacer experiment.")
     runner.execute(config)
-
-    runner = Experimenter(
-        "train" if not config.dummy else "dummy_script",
-        config.python,
-        {
-            "model": "collagan",
-            "adhoc": [
-                "callback-evaluate-fid", "callback-evaluate-l1",
-                "save-model",
-                "all", "no-tran"
-            ],
-            "log-folder": config.output if config.output is not None else "output",
-            "steps": 80000,
-            "evaluate-steps": 1000,
-            "capacity": 1,
-            "lr": 0.0001,
-            "batch": 4,
-            "lambda-l1": 100,
-            "lambda-ssim": 10,
-            "lambda-domain": 10,
-            "lr-decay": "constant-than-linear",
-            "input-dropout": "none",
-            "model-name": "@model",
-            "cycled-source-replacer": "dropout",
-            "experiment": "all,capacity-1"
-        }, {
-        })
-
-    logging.info("Starting the execution of the capacity 1 experiment")
-    runner.execute(config)
+    #
+    # runner = Experimenter(
+    #     "train" if not config.dummy else "dummy_script",
+    #     config.python,
+    #     {
+    #         "model": "collagan",
+    #         "adhoc": [
+    #             "callback-evaluate-fid", "callback-evaluate-l1",
+    #             "save-model",
+    #             "all", "no-tran"
+    #         ],
+    #         "log-folder": config.output if config.output is not None else "output",
+    #         "steps": 80000,
+    #         "evaluate-steps": 1000,
+    #         "capacity": 1,
+    #         "lr": 0.0001,
+    #         "batch": 4,
+    #         "lambda-l1": 100,
+    #         "lambda-ssim": 10,
+    #         "lambda-domain": 10,
+    #         "lr-decay": "constant-than-linear",
+    #         "input-dropout": "none",
+    #         "model-name": "@model",
+    #         "cycled-source-replacer": "dropout",
+    #         "experiment": "all,capacity-1"
+    #     }, {
+    #     })
+    #
+    # logging.info("Starting the execution of the capacity 1 experiment")
+    # runner.execute(config)
