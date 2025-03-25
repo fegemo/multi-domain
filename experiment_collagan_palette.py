5fe7d966c361beba57cd03abff55738df5fe33f0
Flávio Coutinho <fegemo@gmail.com>
2025-03-25 15:41:32 -0300

    Trains experiment_colla_palette longer

--- experiment_collagan_palette.py
+++ experiment_collagan_palette.py
@@ -16,17 +16,17 @@ if __name__ == "__main__":
                 "save-model"
             ],
             "log-folder": config.output if config.output is not None else "output",
-            "steps": 240000,
+            "steps": 480000,
             "evaluate-steps": 1000,
-            "capacity": 4,
-            "lr": 0.0001,
+            "capacity": 20,
+            "lr": 0.00001,
             "ttur": 0.1,
             "lr-decay": "constant-then-linear",
-            "batch": 4,
+            "batch": 20,
             "input-dropout": "conservative",
             "cycled-source-replacer": "forward",
             "model-name": "collagan-palette-coverage",
-            "experiment": "&lambda-palette",
+            "experiment": "lr-1e-5,batch-20,&lambda-palette",
             "generator": "palette",
             "temperature": 0.1,
             "annealing": "linear",
@@ -37,59 +37,7 @@ if __name__ == "__main__":
             "lambda-histogram": 0.,
             "vram": -1,
         }, {
-            "lambda-palette": [1., 0., 0.5],
-        }, {
-            # "tiny": {
-            #     "adhoc": ["no-aug"],
-            # },
-            # "rm2k": {
-            #     "adhoc": ["no-tran"]
-            # },
-            # "rmxp": {
-            #     "adhoc": []
-            # },
-            # "rmvx": {
-            #     "adhoc": ["no-tran"]
-            # },
-            "all": {
-                "adhoc": ["no-tran"],
-            }
-        })
-
-    runner.execute(config)
-
-
-    config = create_general_parser(sys.argv[1:])
-
-    runner = Experimenter(
-        "train" if not config.dummy else "dummy_script",
-        config.python,
-        {
-            "model": "collagan",
-            "adhoc": [
-                "callback-evaluate-fid", "callback-evaluate-l1", #"callback-debug-discriminator",
-                "save-model"
-            ],
-            "log-folder": config.output if config.output is not None else "output",
-            "steps": 240000,
-            "evaluate-steps": 1000,
-            "capacity": 4,
-            "lr": 0.0001,
-            "ttur": 0.1,
-            "lr-decay": "constant-then-linear",
-            "batch": 4,
-            "input-dropout": "conservative",
-            "cycled-source-replacer": "forward",
-            "model-name": "collagan-palette-coverage",
-            "experiment": "original-generator",
-            "lambda-l1": 100.,
-            "lambda-ssim": 1.,
-            "lambda-domain": 10.,
-            "lambda-regularization": 0.001,
-            "lambda-histogram": 0.,
-            "lambda-palette": 0.,
-            "vram": -1,
-        }, {
+            "lambda-palette": [1.],
         }, {
             # "tiny": {
             #     "adhoc": ["no-aug"],
