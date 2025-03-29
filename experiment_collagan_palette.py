c19b5a64e478d1bbf3688d6db12206917debf847
Flávio Coutinho <fegemo@gmail.com>
2025-03-29 15:30:33 -0300

    Updates collagan_palette exp

--- experiment_collagan_palette.py
+++ experiment_collagan_palette.py
@@ -16,17 +16,17 @@ if __name__ == "__main__":
                 "save-model"
             ],
             "log-folder": config.output if config.output is not None else "output",
-            "steps": 320000,
+            "steps": 240000,
             "evaluate-steps": 1000,
             "capacity": 4,
-            "lr": 0.0001,
-            "ttur": 0.75,
+            "lr": 0.00005,
+            "ttur": 0.5,
             "lr-decay": "constant-then-linear",
-            "batch": 12,
+            "batch": 20,
             "input-dropout": "conservative",
             "cycled-source-replacer": "forward",
             "model-name": "collagan-palette-coverage",
-            "experiment": "lr-1e-4,batch-12,ttur-0.75,ssim-10,&lambda-palette,&lambda-adversarial",
+            "experiment": "lr-5e-5,batch-20,ttur-0.5,ssim-10,&lambda-palette,&lambda-adversarial,&lambda-l1",
             "generator": "palette",
             "temperature": 0.1,
             "annealing": "linear",
@@ -37,8 +37,9 @@ if __name__ == "__main__":
             "lambda-histogram": 0.,
             "vram": -1,
         }, {
-            "lambda-palette": [0.1],
-            "lambda-adversarial": [10., 100]
+            "lambda-l1": [200.],
+            "lambda-palette": [0.5, 0],
+            "lambda-adversarial": [10.]
         }, {
             # "tiny": {
             #     "adhoc": ["no-aug"],
