896ae35a4d7efa96295205cf7287da00ffc341a5
Lucas Chaves <gabrielucas2008@hotmail.com>
2025-09-21 11:13:34 -0300

    Adds new CollaGAN experiment with single instance batch and varying learning rate.

---
+++ experiment_collagan_resizing_lr_instance_batch.py
@@ -0,0 +1,54 @@
+import sys
+
+from experiment_runner import Experimenter, create_general_parser
+
+if __name__ == "__main__":
+    config = create_general_parser(sys.argv[1:])
+
+    runner = Experimenter(
+        "train" if not config.dummy else "dummy_script",
+        config.python,
+        {
+            "model": "collagan",
+            "adhoc": [
+                "callback-evaluate-fid",
+                "callback-evaluate-l1",
+                #"callback-debug-discriminator",
+                "save-model",
+            ],
+            "log-folder": config.output if config.output is not None else "output",
+            "steps": 100000,
+            "evaluate-steps": 1000,
+            "capacity": 4,
+            "ttur": 1,
+            "batch": 1,
+            "lr-decay": "constant-then-linear",
+            "input-dropout": "conservative",
+            "cycled-source-replacer": "forward",
+            "model-name": "@model",
+            "experiment": "resizing,@dataset,&adhoc,&resizing-factor",
+            "generator": "affluent",
+            "lambda-adversarial": 1.,
+            "lambda-l1": 100.,
+            "lambda-ssim": 1.,
+            "lambda-domain": 10.,
+            "lambda-regularization": 0.001,
+            "vram": -1
+        },
+        {
+            "adhoc": [
+                ["up-preprocessing", "no-up-aug"], #só pre-processamento
+                [], #só augmentation
+                ["up-preprocessing"], #ambos
+            ],
+            "resizing-factor": [2, 3],
+            "lr": [0.00005, 0.00001]
+        },
+        {
+            "all": {
+                "adhoc": ["no-tran"]
+            }
+        }
+    )
+
+    runner.execute(config)
