4692b1956f149f3d88a8d43a5bb76e44952b061f
Flávio Coutinho <fegemo@gmail.com>
2025-09-22 09:35:34 -0300

    Makes --inpaint-mask none as default for SpriteGAN

--- configuration.py
+++ configuration.py
@@ -165,7 +165,7 @@ class OptionParser(metaclass=SingletonMeta):
                                  default=False)
         self.parser.add_argument("--perturb-palette", type=float, default=0.0,
                                  help="The probability with which to perturb the target palette when training CollaGAN")
-        self.parser.add_argument("--inpaint-mask", default="curriculum",
+        self.parser.add_argument("--inpaint-mask", default="none",
                                  help="one from {none, constant, random, curriculum} defining how the inpaint mask "
                                       "is generated in SpriteGAN.")
         self.parser.add_argument("--sampler", help="one from {multi-target, single-target} indicating whether "
